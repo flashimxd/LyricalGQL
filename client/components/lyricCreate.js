f7abe3ff3b42f202f4ee0da93577b95e856cfadd
Rangel Netto <j.rangel.flash@gmail.com>
2019-02-14 12:01:11 +0000

    new mutations

--- client/components/lyricCreate.js
+++ client/components/lyricCreate.js
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 class lyricCreate extends Component{
+    constructor(props){
+        super(props)
+        this.state = {content: ''}
+    }
+    submitHandler(evt) {
+        evt.preventDefault()
+        const {content} = this.state
+        const {songId} = this.props
+        this.props.mutate({
+            variables: {
+                content,
+                songId
+            }
+        }).then(() => this.setState({content: ''}))
+    }
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit={this.submitHandler.bind(this)}>
                     <label> Add Lyric</label>
-                    <input />
+                    <input value={this.state.content} onChange={evt => this.setState({ content: evt.target.value })}/>
                 </form>
             </div>
         );
     }
 }
 
-export default lyricCreate;
+const mutation = gql`
+    mutation AddLyricToSong($content: String, $songId: ID) {
+        addLyricToSong(content: $content ,songId: $songId) {
+            id,
+            lyrics{
+                id,
+                content
+            }
+        }
+    }
+`
+export default graphql(mutation)(lyricCreate)
--- client/components/songDetail.js
+++ client/components/songDetail.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 import {graphql} from 'react-apollo'
 import { Link } from 'react-router'
 import fetchSong from '../queries/fetchSong'
+import LyricCreate from './lyricCreate'
 
-class songDetail extends Component{
+class songDetail extends Component {
     render(){
         const { song } = this.props.data
+        const { id } = this.props.params
+
         if(!song) return (<div>Loading...</div>)
 
         return (
             <div>
                 <Link to='/'>Back</Link>
                 <h3>{song.title}</h3>
+                <LyricCreate songId={id} />
             </div>
         )
     }
